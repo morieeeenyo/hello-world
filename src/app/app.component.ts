c8b85494136298e4b0a9f4ea6656edc76839ec42
togo-mentor <uint2435@gmail.com>
2021-12-02 08:08:19 +0900

    変数定義と条件式

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html', // このコンポーネントに対応するhtmlファイル
+  styleUrls: ['./app.component.css'] // このコンポーネントに対応するcssファイル
 })
+
 export class AppComponent {
-  title = 'hello-world';
+  // 変数を定義する
+  title = 'Hello World';
+  isAngular=true
+  string = 'angular'
 }
